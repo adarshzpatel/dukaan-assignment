eb9f60dd42ba67f6c15263e2b1ced36ec697fb7e
adarshzpatel <adarshzpatel@gmail.com>
2024-01-09 07:32:49 +0530

    fixed icon

--- src/assets/icons/ArrowDownFilledIcon.tsx
+++ src/assets/icons/ArrowDownFilledIcon.tsx
@@ -10,7 +10,6 @@ const ArrowDownFilledIcon = ({...props}) => {
       {...props}
     >
       <g clipPath="url(#clip0_35241_2537)">
-        <circle cx="20" cy="20" r="20" fill="#E6E6E6"></circle>
         <path
           fill="#4D4D4D"
           d="M21.11 25.503a1.5 1.5 0 01-2.22 0l-5.275-5.814c-.874-.963-.19-2.507 1.111-2.507h10.548c1.302 0 1.985 1.544 1.11 2.508l-5.273 5.813z"
